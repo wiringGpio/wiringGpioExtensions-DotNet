9febf1a25d98b9d30e754e8bda4b8d12ce794772
Graham Briggs <GrahamBriggs@LittleBytesOfPi.com>
2022-12-02 17:01:59 -0800

    Added definition for log event.

---
+++ LogEvent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Runtime.InteropServices.Marshal;
+
+namespace wiringGpioExtensions
+{
+
+    public class LogEvent
+    {
+        public static DateTime UnixTimeStampToDateTime(long unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddMilliseconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+
+
+        public LogEvent(WiringGpioLogEvent e)
+        {
+            LogTime = UnixTimeStampToDateTime(e.LogUnixTimeMilliseconds);
+            ObjectName = PtrToStringAnsi(e.SenderNamePtr);
+            FunctionName = PtrToStringAnsi(e.FunctionNamePtr);
+            Log = PtrToStringAnsi(e.DataPtr);
+            Level = e.Level;
+        }
+
+        public DateTimeOffset LogTime { get; protected set; }
+        public string ObjectName { get; protected set; }
+        public string FunctionName { get; protected set; }
+        public string Log { get; protected set; }
+        public Logging.LogLevel Level;
+
+
+    }
+}
